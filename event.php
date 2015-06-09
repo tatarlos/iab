c236eb480cbb12a029eebaccaa599b629d321604
tatarlos <haochen.liu6487@1613c006.mediadesign.internal>
2015-06-09 15:20:47 +1200

    restart php v1

--- event.php
+++ event.php
@@ -13,6 +13,18 @@ $type = $cat."_type.name";
 
 <?php get_header(); ?>
 
+
+<nav >
+ <ul class="clearfix">
+
+    <li><a href="<?php echo bloginfo('url')?>/news">NEWS</a></li>
+    <li><a href="<?php echo bloginfo('url')?>/events">EVENTS</a></li>
+    <li><a href="<?php echo bloginfo('url')?>/resources">RESOURCES</a></li>
+    <li><a href="<?php echo bloginfo('url')?>/members">MEMBERS</a></li>
+    <li><a href="<?php echo bloginfo('url')?>/about">ABOUT IAB</a></li>
+ </ul>
+</nav>
+
 <main class="wrapper clearfix">
     <h2><?php echo $eventsPod->field('title'); ?></h2>
     <div class="event">
@@ -26,10 +38,6 @@ $type = $cat."_type.name";
         <?php 
             $postId = $eventsPod->field('ID');
             $image_id = get_post_thumbnail_id($postId);
-            // echo "<br>Categories: ".$eventsPod->display('category')."<br>";          
-            // $categories = wp_get_object_terms($postId,'category');
-
-
             $image = wp_get_attachment_image_src($image_id,'large');
             $image_url = $image[0];
 
